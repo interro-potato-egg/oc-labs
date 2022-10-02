7704cdc8c70dfe4f1adf08992a809ba4018000f7
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-10-02 19:19:16 +0100

    Fix 2.3.3 AMAT calculation, change answers to .ipynb from .md

--- lab-1/lab1_kit/2.3.2/cenas.sh
+++ lab-1/lab1_kit/2.3.2/cenas.sh
@@ -5,13 +5,15 @@
 
 echo "" > results.txt
 
+FRAME_MEMORY_COST=0.00125
+
 for BYTE in 256 512 1024
 do
     for BLOCK_SIZE in 8 16 32 64
     do
         echo "Running for BYTE = $BYTE and BLOCK_SIZE = $BLOCK_SIZE" >> results.txt
         MISS_RATE=$(../../d4-7/dineroIV -l1-dsize $BYTE -l1-dbsize $BLOCK_SIZE -l1-dccc < ../trace.log | rg "Demand miss rate" | cut -d" " -f11)
-        PRICE=$(echo "scale=10; 10/(1024*1024) * $BYTE" | bc)
+        PRICE=$(echo "scale=10; $FRAME_MEMORY_COST + (10/(1024*1024) * $BYTE)" | bc)
         COST=$(echo "scale=10; $PRICE * $MISS_RATE" | bc)
         echo "MISS_RATE: $MISS_RATE" >> results.txt
         echo "PRICE: $PRICE" >> results.txt
@@ -19,10 +21,8 @@ do
     done
 done
 
-# from results.txt, get the MISS_RATE, PRICE and COST for the two cheapest COST values
-
 CHEAPEST_COST=$(cat results.txt | rg "COST" | cut -d" " -f2 | sort -n | head -n1)
 SECOND_CHEAPEST_COST=$(cat results.txt | rg "COST" | cut -d" " -f2 | sort -n | head -n2 | tail -n1)
 
-echo "CHEAPEST_COST: $CHEAPEST_COST"
-echo "SECOND_CHEAPEST_COST: $SECOND_CHEAPEST_COST"
+echo "CHEAPEST_COST: $CHEAPEST_COST" >> results.txt
+echo "SECOND_CHEAPEST_COST: $SECOND_CHEAPEST_COST" >> results.txt
--- lab-1/lab1_kit/2.3.3/cenas.sh
+++ lab-1/lab1_kit/2.3.3/cenas.sh
@@ -2,17 +2,16 @@
 
 echo "" > results.txt
 
-BYTE=1024
-PRICE=$(echo "scale=10; 10/$BYTE + 0.01/8" | bc)
+CACHE_SIZE=1024
+PRICE=$(echo "scale=10; 10/$CACHE_SIZE + 0.00125" | bc)
 
 for WAYS in 1 2 4 8
 do
     for BLOCK_SIZE in 8 16
     do
-        echo "Running for $BYTE B, BLOCK_SIZE = $BLOCK_SIZE and WAYS = $WAYS" >> results.txt
+        echo "Running for $CACHE_SIZE B, BLOCK_SIZE = $BLOCK_SIZE and WAYS = $WAYS" >> results.txt
         DINERO=$(../../d4-7/dineroIV -l1-dsize 1024 -l1-dbsize $BLOCK_SIZE -l1-dassoc $WAYS -l1-dccc < ../trace.log)
         MISS_RATE=$(echo "$DINERO" | rg "Demand miss rate" | cut -d" " -f11)
-        TOTAL_MISS_RATE=0
         for MISS_TYPE in "Compulsory" "Conflict" "Capacity"
         do
             FRACTION=$(echo "$DINERO" | rg "$MISS_TYPE fraction" | cut -f2)
@@ -20,11 +19,10 @@ do
             TYPE_MISS_RATE=$(echo "scale=10; $MISS_RATE * $FRACTION" | bc)
             echo "$MISS_TYPE miss rate, with $WAYS ways: $TYPE_MISS_RATE" >> results.txt
             echo "$MISS_TYPE RELATIVE miss rate (% among types), with $WAYS ways: $PERCENTAGE" >> results.txt
-            TOTAL_MISS_RATE=$(echo "scale=10; $TOTAL_MISS_RATE + $TYPE_MISS_RATE" | bc)
         done
-        echo "Total miss rate, with $WAYS ways: $TOTAL_MISS_RATE" >> results.txt
-        AMAT_RIGHT=$(echo "scale=10; $TOTAL_MISS_RATE * 140" | bc)
-        AMAT_LEFT=$(echo "scale=10; 2 * (0.7 + 0.35 * (l($WAYS) / l(10)))" | bc -l)
+        echo "Total miss rate, with $WAYS ways: $MISS_RATE" >> results.txt
+        AMAT_RIGHT=$(echo "scale=10; $MISS_RATE * 140" | bc)
+        AMAT_LEFT=$(echo "scale=10; 2 * (0.7 + 0.35 * l($WAYS) / l(2))" | bc -l)
         AMAT=$(echo "scale=10; $AMAT_LEFT + $AMAT_RIGHT" | bc)
         echo "AMAT, with $WAYS ways: $AMAT" >> results.txt
         COST=$(echo "scale=10; $PRICE * $AMAT" | bc) # from EQ.1, as suggested in the question's statement
