fe094d1907e5c41d7446c8d297729014ba78cd06
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-10-02 22:32:04 +0100

    Fix 2.4.2 inconsistencies, still not done

--- lab-1/lab1_kit/2.4.2d/cenas.sh
+++ lab-1/lab1_kit/2.4.2d/cenas.sh
@@ -16,13 +16,13 @@ L1_MISS_RATE=$(cat dump.log | rg -m1 "Demand miss rate" | cut -d" " -f11)
 L2_MISS_RATE=$(cat dump.log | rg "Demand miss rate" | tail -n1 | cut -d" " -f11)
 echo "L1 miss rate: $L1_MISS_RATE" >> results.txt
 echo "L2 miss rate: $L2_MISS_RATE" >> results.txt
-TOTAL_MISS_RATE=$(echo "scale=10; $L1_MISS_RATE * $L2_MISS_RATE" | bc -l)
-echo "TOTAL MISS RATE: $TOTAL_MISS_RATE" >> results.txt
+COMBINED_MISS_RATE=$(echo "scale=10; $L1_MISS_RATE * $L2_MISS_RATE" | bc -l)
+echo "COMBINED MISS RATE: $COMBINED_MISS_RATE" >> results.txt
 L1_HIT_TIME=$(echo "scale=10; 2 * (0.7 + 0.35 * l($WAYS) / l(2))" | bc -l)
 L2_HIT_TIME=$(echo "scale=10; 10 * (0.7 + 0.55 * l($WAYS) / l(2))" | bc -l)
 echo "L1 hit time: $L1_HIT_TIME" >> results.txt
 echo "L2 hit time: $L2_HIT_TIME" >> results.txt
-AMAT=$(echo "scale=10; $L1_HIT_TIME + ($L1_MISS_RATE * $L2_HIT_TIME) + ($L1_MISS_RATE * $L2_MISS_RATE * $MEMORY_TIME)" | bc -l)
+AMAT=$(echo "scale=10; $L1_HIT_TIME + ($L1_MISS_RATE * $L2_HIT_TIME) + ($COMBINED_MISS_RATE * $MEMORY_TIME)" | bc -l)
 COST=$(echo "scale=10; $AMAT * $PRICE" | bc -l)
 echo "AMAT: $AMAT" >> results.txt
 echo "PRICE: $PRICE" >> results.txt
