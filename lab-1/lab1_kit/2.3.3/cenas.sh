1b16b8ce4f01f8d7f5d70b36dc0ac7cc6b88b712
Rafael Oliveira <rafdev.x@gmail.com>
2022-10-04 12:56:46 +0100

    review up to 2.3.3.d) (inclusive)

--- lab-1/lab1_kit/2.3.3/cenas.sh
+++ lab-1/lab1_kit/2.3.3/cenas.sh
@@ -1,20 +1,22 @@
 #/bin/sh
 
+set -e
+
 echo "" > results.txt
 
 CACHE_SIZE=1024
-PRICE=$(echo "scale=10; 10/$CACHE_SIZE + 0.00125" | bc)
+PRICE=$(echo "scale=10; (10*$CACHE_SIZE / 2^20) + 0.00125" | bc)
 
 for WAYS in 1 2 4 8
 do
     for BLOCK_SIZE in 8 16
     do
-        echo "Running for $CACHE_SIZE B, BLOCK_SIZE = $BLOCK_SIZE and WAYS = $WAYS" >> results.txt
+        echo "Running for $CACHE_SIZE B, BLOCK_SIZE = $BLOCK_SIZE and WAYS = $WAYS" | tee -a results.txt
         DINERO=$(../../d4-7/dineroIV -l1-dsize 1024 -l1-dbsize $BLOCK_SIZE -l1-dassoc $WAYS -l1-dccc < ../trace.log)
-        MISS_RATE=$(echo "$DINERO" | rg "Demand miss rate" | cut -d" " -f11)
+        MISS_RATE=$(echo "$DINERO" | grep "Demand miss rate" | cut -d" " -f11)
         for MISS_TYPE in "Compulsory" "Conflict" "Capacity"
         do
-            FRACTION=$(echo "$DINERO" | rg "$MISS_TYPE fraction" | cut -f2)
+            FRACTION=$(echo "$DINERO" | grep "$MISS_TYPE fraction" | cut -f2)
             PERCENTAGE=$(echo "$FRACTION * 100" | bc -l)
             TYPE_MISS_RATE=$(echo "scale=10; $MISS_RATE * $FRACTION" | bc)
             echo "$MISS_TYPE miss rate, with $WAYS ways: $TYPE_MISS_RATE" >> results.txt
