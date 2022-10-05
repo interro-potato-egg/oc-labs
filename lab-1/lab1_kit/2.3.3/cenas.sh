7e2f632c09671340d419171c3f27c3d8564264fd
Rafael Oliveira <rafdev.x@gmail.com>
2022-10-05 16:38:03 +0100

    use relative type miss rates instead of absolute

--- lab-1/lab1_kit/2.3.3/cenas.sh
+++ lab-1/lab1_kit/2.3.3/cenas.sh
@@ -7,17 +7,17 @@ echo "" > results.txt
 CACHE_SIZE=1024
 PRICE=$(echo "scale=10; (10*$CACHE_SIZE / 2^20) + 0.00125" | bc)
 
-for WAYS in 1 2 4 8
+for BLOCK_SIZE in 8 16
 do
-    for BLOCK_SIZE in 8 16
+    for WAYS in 1 2 4 8
     do
         echo "Running for $CACHE_SIZE B, BLOCK_SIZE = $BLOCK_SIZE and WAYS = $WAYS" | tee -a results.txt
         DINERO=$(../../d4-7/dineroIV -l1-dsize 1024 -l1-dbsize $BLOCK_SIZE -l1-dassoc $WAYS -l1-dccc < ../trace.log)
         MISS_RATE=$(echo "$DINERO" | grep "Demand miss rate" | cut -d" " -f11)
-        for MISS_TYPE in "Compulsory" "Conflict" "Capacity"
+        for MISS_TYPE in "Compulsory" "Capacity" "Conflict"
         do
             FRACTION=$(echo "$DINERO" | grep "$MISS_TYPE fraction" | cut -f2)
-            PERCENTAGE=$(echo "$FRACTION * 100" | bc -l)
+            PERCENTAGE=$(echo "$FRACTION" | bc) # trim number
             TYPE_MISS_RATE=$(echo "scale=10; $MISS_RATE * $FRACTION" | bc)
             echo "$MISS_TYPE miss rate, with $WAYS ways: $TYPE_MISS_RATE" >> results.txt
             echo "$MISS_TYPE RELATIVE miss rate (% among types), with $WAYS ways: $PERCENTAGE" >> results.txt
