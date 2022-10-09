8af7b995179254476ab42314aa535aeddf2842e8
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2022-10-09 17:35:22 +0100

    Add lab-2 boilerplate stuff

---
+++ lab-1/lab1_kit/2.3.3/cenas.sync-conflict-20221006-222203-IPEL5DI.sh
@@ -0,0 +1,36 @@
+#/bin/sh
+
+set -e
+
+echo "" > results.txt
+
+CACHE_SIZE=1024
+PRICE=$(echo "scale=10; (10*$CACHE_SIZE / 2^20) + 0.00125" | bc)
+
+for WAYS in 1 2 4 8
+do
+    for BLOCK_SIZE in 8 16
+    do
+        echo "Running for $CACHE_SIZE B, BLOCK_SIZE = $BLOCK_SIZE and WAYS = $WAYS" | tee -a results.txt
+        DINERO=$(../../d4-7/dineroIV -l1-dsize 1024 -l1-dbsize $BLOCK_SIZE -l1-dassoc $WAYS -l1-dccc < ../trace.log)
+        MISS_RATE=$(echo "$DINERO" | grep "Demand miss rate" | cut -d" " -f11)
+        for MISS_TYPE in "Compulsory" "Conflict" "Capacity"
+        do
+            FRACTION=$(echo "$DINERO" | grep "$MISS_TYPE fraction" | cut -f2)
+            PERCENTAGE=$(echo "$FRACTION * 100" | bc -l)
+            TYPE_MISS_RATE=$(echo "scale=10; $MISS_RATE * $FRACTION" | bc)
+            echo "$MISS_TYPE miss rate, with $WAYS ways: $TYPE_MISS_RATE" >> results.txt
+            echo "$MISS_TYPE RELATIVE miss rate (% among types), with $WAYS ways: $PERCENTAGE" >> results.txt
+        done
+        echo "Total miss rate, with $WAYS ways: $MISS_RATE" >> results.txt
+        AMAT_RIGHT=$(echo "scale=10; $MISS_RATE * 140" | bc)
+        AMAT_LEFT=$(echo "scale=10; 2 * (0.7 + 0.35 * l($WAYS) / l(2))" | bc -l)
+        AMAT=$(echo "scale=10; $AMAT_LEFT + $AMAT_RIGHT" | bc)
+        echo "AMAT, with $WAYS ways: $AMAT" >> results.txt
+        COST=$(echo "scale=10; $PRICE * $AMAT" | bc) # from EQ.1, as suggested in the question's statement
+        echo "COST: $COST" >> results.txt
+        echo "---" >> results.txt
+    done
+done
+
+echo "Price, for both configs, is $PRICE" >> results.txt
