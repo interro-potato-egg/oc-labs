05fc86dfcf891702fa0c676cae99c372e4143670
Rafael Oliveira <rafdev.x@gmail.com>
2022-10-25 19:02:26 +0100

    improve 2.4 (loop unrolling)

--- lab-3/lab3_kit/2.4prog.s
+++ lab-3/lab3_kit/2.4prog.s
@@ -6,26 +6,24 @@ mult:   .word   0
         .code
         daddi   $1, $0, A      ; *A[0]
         daddi   $5, $0, 1      ; $5 = 1 ;; i    
-        daddi   $6, $0, 9     ; $6 = N ;; N = 10
+        daddi   $6, $0, 9      ; $6 = N - 1 ;; N = 10
         lw      $9, 0($1)      ; $9 = A[0]  ;; mult
-        daddi   $1, $1, 8      ;
+        lw      $12, 8($1)     ; $12 = A[i]
 
-loop:   lw      $12, 0($1)     ; $12 = A[i]
-        dmul    $12, $12, $9   ; $12 = $12*$9 ;; $12 = A[i]*mult
-        dadd    $9, $9, $12    ; $9 = $9 + $12  ;; mult = mult + A[i]*mult
-
-        lw      $12, 8($1)     ; $12 = A[i+1]
+loop:   dmul    $12, $12, $9   ; $12 = A[i]*mult
         daddi   $5, $5, 2      ; i += 2
-        dmul    $12, $12, $9   ; $12 = $12*$9 ;; $12 = A[i+1]*mult
-        daddi   $1, $1, 16     ; 
-        dadd    $9, $9, $12    ; $9 = $9 + $12  ;; mult = mult + A[i+1]*mult
+        lw      $22, 16($1)    ; $12 = A[i+1]
+        dadd    $9, $9, $12    ; mult = mult + A[i]*mult
 
-        bne     $6, $5, loop   ; Exit loop if i == N
+        dmul    $22, $22, $9   ; $22 = A[i+1]*mult
+        daddi   $1, $1, 16     ; 
+        lw      $12, 8($1)     ; $12 = A[i+2]
+        dadd    $9, $9, $22    ; mult = mult + A[i+1]*mult
+        bne     $6, $5, loop   ; Exit loop if i == (N-1)
 
         ; 9 iters - missing 1
-        lw      $12, 0($1)     ; $12 = A[i]
-        dmul    $12, $12, $9   ; $12 = $12*$9 ;; $12 = A[i]*mult
-        dadd    $9, $9, $12    ; $9 = $9 + $12  ;; mult = mult + A[i]*mult
+        dmul    $12, $12, $9   ; $12 = A[i]*mult
+        dadd    $9, $9, $12    ; mult = mult + A[i]*mult
         
         sw      $9, mult($0)   ; Store result
         halt
