a727c97f40acd8326880445345eed8991c9c2f67
Rafael Oliveira <rafdev.x@gmail.com>
2022-10-25 01:18:14 +0100

    continue up to 2.4 (inclusive)

---
+++ lab-3/lab3_kit/2.4prog.s
@@ -0,0 +1,34 @@
+                .data
+A:              .word   1, 3, 1, 6, 4
+                .word   2, 4, 3, 9, 5
+mult:   .word   0
+
+        .code
+        daddi   $1, $0, A      ; *A[0]
+        daddi   $5, $0, 1      ; $5 = 1 ;; i    
+        daddi   $6, $0, 9     ; $6 = N ;; N = 10
+        lw      $9, 0($1)      ; $9 = A[0]  ;; mult
+        daddi   $1, $1, 8      ;
+
+loop:   lw      $12, 0($1)     ; $12 = A[i]
+        dmul    $12, $12, $9   ; $12 = $12*$9 ;; $12 = A[i]*mult
+        dadd    $9, $9, $12    ; $9 = $9 + $12  ;; mult = mult + A[i]*mult
+
+        lw      $12, 8($1)     ; $12 = A[i+1]
+        daddi   $5, $5, 2      ; i += 2
+        dmul    $12, $12, $9   ; $12 = $12*$9 ;; $12 = A[i+1]*mult
+        daddi   $1, $1, 16     ; 
+        dadd    $9, $9, $12    ; $9 = $9 + $12  ;; mult = mult + A[i+1]*mult
+
+        bne     $6, $5, loop   ; Exit loop if i == N
+
+        ; 9 iters - missing 1
+        lw      $12, 0($1)     ; $12 = A[i]
+        dmul    $12, $12, $9   ; $12 = $12*$9 ;; $12 = A[i]*mult
+        dadd    $9, $9, $12    ; $9 = $9 + $12  ;; mult = mult + A[i]*mult
+        
+        sw      $9, mult($0)   ; Store result
+        halt
+
+;; Expected result: mult = f6180 (hex), 1008000 (dec)
+
