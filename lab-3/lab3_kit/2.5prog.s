dd515f080d83ae5c5aaa3f4eb6bbf87a3d18dfaf
Rafael Oliveira <rafdev.x@gmail.com>
2022-10-25 19:30:45 +0100

    dynamically calculate and display speedup

--- lab-3/lab3_kit/2.5prog.s
+++ lab-3/lab3_kit/2.5prog.s
@@ -7,7 +7,7 @@ mult:   .word   0
         daddi   $1, $0, A      ; *A[0]
         daddi   $5, $0, 1      ; $5 = 1 ;; i
         daddi   $6, $0, 10     ; $6 = N ;; N = 10
-        lw      $9, 0($1)      ; $9 = A[0]  ;; mult
+        lw      $9, 0($1)      ; $9 = A[0] ;; mult
         daddi   $1, $1, 8      ;
 
 loop:   lw      $12, 0($1)     ; $12 = A[i]
@@ -16,7 +16,7 @@ loop:   lw      $12, 0($1)     ; $12 = A[i]
         daddi   $1, $1, 8      ;
 
         bne     $6, $5, loop   ; Exit loop if i == N
-        dadd    $9, $9, $12    ; $9 = $9 + $12  ;; mult = mult + A[i]*mult
+        dadd    $9, $9, $12    ; $9 = $9 + $12 ;; mult = mult + A[i]*mult
 
         sw      $9, mult($0)   ; Store result
         halt
